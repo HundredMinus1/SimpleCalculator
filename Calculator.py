9f40d7bb077bf0b5cc290918dd83235eac5eb353
Habib Septrian <habibseptrian@gmail.com>
2022-11-01 10:17:29 +0700

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+
+
+C = "y"
+
+while C == "y":
+    print("Simple Calculator 1.0")
+    print("+-*/ for the operations")
+    A = int(input("Enter your first number here: "))
+    Opera = input("Enter your operation here: ")
+    B = int(input("Enter your second number here: "))
+   
+    if Opera == "+":
+        print("Result: ", A+B)
+        C = input("Exit calculator? (y/n) ")
+    elif Opera == "-":
+        print("Result: ", A-B)
+        C = input("Exit calculator? (y/n) ")
+    elif Opera == "*":
+        print("Result: ", A*B)
+        C = input("Exit calculator? (y/n) ")
+    elif Opera == "/":
+        print("Result: ", A/B)
+        C = input("Exit calculator? (y/n) ")
+
+    
+
+
